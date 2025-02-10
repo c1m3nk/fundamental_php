aac73972345d0b34dc44670a90aaad6d58b3a5da
c1m3nk <whosme1226@gmail.com>
2025-02-10 13:35:11 +0700

    add

--- dasar php/index.php
+++ dasar php/index.php
@@ -6,16 +6,26 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    // $cuan = "sakmene cah";
-    // echo "duwek ku " .$cuan."<br/>";
+    <?php 
 
-    // define ("PHI", 3.14);
-    // echo "luas lingkaran dengan jari jari 7 adalah" .PHI*7*7;
+    // awal php
 
-    // bentar bree;
-    ?>
+    $cuan = 100000;
+    echo "duwek ku kari sakmene " .number_format($cuan,0,',','.')."<br/>";
+
+    // materi konstanta
+
+    define ("PHI", 3.14);
+    echo "luas lingkaran dengan jari jari 7 adalah <br/>" .PHI*7*7;
+
+    // percabangan satu kondisi
+
+    $bil = 25 ;
+    if ($bil % 5 == 0) {
+        echo "bilangan $bil adalah kelipatan 5 ";
+    }
      
+?>
 
 </body>
 </html>
