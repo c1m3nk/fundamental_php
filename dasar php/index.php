365b8ea80cbec7179c6d331ecd9dfe7cddaabc85
c1m3nk <whosme1226@gmail.com>
2025-02-12 17:06:43 +0700

    delet

--- dasar php/index.php
+++ dasar php/index.php
@@ -6,26 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
+   
 
-    // awal php
-
-    $cuan = 100000;
-    echo "duwek ku kari sakmene " .number_format($cuan,0,',','.')."<br/>";
-
-    // materi konstanta
-
-    define ("PHI", 3.14);
-    echo "luas lingkaran dengan jari jari 7 adalah <br/>" .PHI*7*7;
-
-    // percabangan satu kondisi
-
-    $bil = 25 ;
-    if ($bil % 5 == 0) {
-        echo "bilangan $bil adalah kelipatan 5 ";
-    }
-     
-?>
 
 </body>
 </html>
