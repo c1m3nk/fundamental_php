d91baba5ca8a561dc4e87a317c50073275a9fa29
c1m3nk <whosme1226@gmail.com>
2025-02-12 11:42:59 +0700

    add

--- tugas sekolah php/per2/hal2form.php
+++ tugas sekolah php/per2/hal2form.php
@@ -6,6 +6,7 @@
     <title>pra daftar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
+        
       table {
         display:absolute;
         justify-content: center;
